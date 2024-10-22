f49be601291e142250828fa85f8839f2e6b3d8d1
LovelySingh99 <lovely@metadrob.com>
2024-10-22 15:08:40 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connectToMongo() {
     try {
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect("mongodb+srv://jainchaitanya1024:Chaitanyamongo123@clusterigm.0wahhnv.mongodb.net/?retryWrites=true&w=majority&appName=ClusterIGM");
         console.log('Connected to MongoDB successfully!');
     } catch (err) {
         console.error('Error connecting to MongoDB:', err);
